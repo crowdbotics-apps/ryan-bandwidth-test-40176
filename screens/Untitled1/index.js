31941376794866460210e974dfaecc513550ea2c
Ryan Brill <brill@crowdbotics.com>
2023-04-24 13:44:16 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,3 +1,4 @@
+import { RadioGroup } from "react-native-radio-buttons-group";
 import { TextInput } from "react-native";
 import { Text } from "react-native";
 import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
@@ -10,7 +11,15 @@ const Untitled1 = () => {
       position: 'relative',
       flex: 1
     }}><View style={styles.YvRwmBqI}><View style={styles.QWzqXGnc}><View style={styles.tMaemcDC}><Text style={styles.HgtrBtiz}>Network Tester
-     </Text><Text style={styles.uMbtUPsF}>Welcome</Text><View style={styles.MGGDlegA}><TextInput style={styles.xVhaeCrQ} numberOfLines={1} autoFocus={true} editable={true} placeholder="Email Address" selectionColor="#c4f3fd"></TextInput></View></View></View></View></ScrollView>
+     </Text><Text style={styles.uMbtUPsF}>Welcome</Text><View style={styles.MGGDlegA}><TextInput style={styles.xVhaeCrQ} numberOfLines={1} autoFocus={true} editable={true} placeholder="Email Address" selectionColor="#c4f3fd"></TextInput><View style={styles.BTzQSrdj}></View><Text style={styles.zrrXPKIx}>Network Connection </Text><RadioGroup style={styles.PRZXcrfu} radioButtons={[{
+                id: "1",
+                label: "Option 1",
+                value: "option1"
+              }, {
+                id: "2",
+                label: "Option 2",
+                value: "option2"
+              }]} layout="row"></RadioGroup></View></View></View></View></ScrollView>
     </SafeAreaView>;
 };
 
@@ -79,6 +88,35 @@ const styles = StyleSheet.create({
     height: 30,
     position: "absolute",
     left: 52.5
+  },
+  BTzQSrdj: {
+    height: 33,
+    width: 158,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777",
+    position: "absolute",
+    top: 78,
+    left: 50
+  },
+  zrrXPKIx: {
+    width: 152,
+    height: 22,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    position: "absolute",
+    top: 149,
+    left: 54,
+    color: "#8593A6",
+    textAlign: "left"
+  },
+  PRZXcrfu: {
+    width: 192,
+    height: 70,
+    position: "relative",
+    top: 201,
+    left: 56
   }
 });
 export default Untitled1;
